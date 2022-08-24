8b9331d29d1ac3ec9cb593d853bfd5068ed590f6
AlessioCalabrese1 <alessiocalabrese07@gmail.com>
2022-08-24 17:44:19 +0200

    Footer implementation

--- index.php
+++ index.php
@@ -1,17 +1,17 @@
 <?php
-    $headerLinks =[
-        "Introduzione",
-        "Norme sulla privacy",
-        "Termini di servizio",
-        "Tecnologie",
-        "Domande frequenti"
-    ];
+$headerLinks = [
+    "Introduzione",
+    "Norme sulla privacy",
+    "Termini di servizio",
+    "Tecnologie",
+    "Domande frequenti"
+];
 
-    $database = [
-        [
-            "faq" => "Come state implementando la recente decisione della Corte di giustizia dell'Unione europea (CGUE) relativa al diritto all'oblio?",
+$database = [
+    [
+        "faq" => "Come state implementando la recente decisione della Corte di giustizia dell'Unione europea (CGUE) relativa al diritto all'oblio?",
 
-            "response" => "<div class='my-4'> La recente <a href='#'>decisione della Corte di giustizia dell'Unione europea</a> ha profonde conseguenze per i motori di ricerca in Europa. La Corte ha stabilito che alcuni utenti hanno il diritto di chiedere ai motori di ricerca come Google di rimuovere risultati relativi a chiavi di ricerca che includono il proprio nome. Per poter essere rimossi, i risultati visualizzati devono essere inadeguati, irrilevanti o non più rilevanti, o eccessivi.</div> <div class='my-4'>
+        "response" => "<div class='my-4'> La recente <a href='#'>decisione della Corte di giustizia dell'Unione europea</a> ha profonde conseguenze per i motori di ricerca in Europa. La Corte ha stabilito che alcuni utenti hanno il diritto di chiedere ai motori di ricerca come Google di rimuovere risultati relativi a chiavi di ricerca che includono il proprio nome. Per poter essere rimossi, i risultati visualizzati devono essere inadeguati, irrilevanti o non più rilevanti, o eccessivi.</div> <div class='my-4'>
 
             Da quando questa decisione è stata pubblicata il 13 maggio 2014, abbiamo lavorato incessantemente per uniformarci a essa. Si tratta di una procedura complessa perché dobbiamo valutare ogni singola richiesta e effettuare un bilanciamento tra il diritto dell'individuo a controllare i suoi dati personali ed il diritto di tutti di conoscere e distribuire le informazioni.</div> <div class='my-4'>
             
@@ -20,22 +20,22 @@
             Nei prossimi mesi lavoreremo a stretto contatto con le autorità per la protezione dei dati e con altre autorità per perfezionare il nostro approccio. La decisione della CGUE rappresenta un cambiamento significativo per i motori di ricerca. Siamo preoccupati per le sue conseguenze, ma riteniamo anche che sia importante rispettare la decisione della Corte e ci stiamo adoperando per predisporre una procedura conforme alla legge.</div> <div class='my-4'>
             
             Quando cerchi un nome, potresti trovare una notifica in cui viene spiegato che i risultati potrebbero essere stati modificati nel rispetto delle leggi europee per la protezione dei dati. Mostriamo questa notifica agli utenti europei quando cercano la maggior parte dei nomi, non soltanto le pagine che sono state soggette a una rimozione.</div>"
-        ],
-        [
-            "faq" => "Come fa Google a proteggere la mia privacy e a tenere le mie informazioni al sicuro?",
+    ],
+    [
+        "faq" => "Come fa Google a proteggere la mia privacy e a tenere le mie informazioni al sicuro?",
 
-            "response" => "<div class='my-4'> Sappiamo che la sicurezza e la privacy sono importanti per te e lo sono anche per noi. Per noi è imperativo offrire un'elevata sicurezza e assicurarti che le tue informazioni sono al sicuro e accessibili quando ne hai bisogno.</div> <div class='my-4'>
+        "response" => "<div class='my-4'> Sappiamo che la sicurezza e la privacy sono importanti per te e lo sono anche per noi. Per noi è imperativo offrire un'elevata sicurezza e assicurarti che le tue informazioni sono al sicuro e accessibili quando ne hai bisogno.</div> <div class='my-4'>
 
             Siamo costantemente al lavoro per garantire un'elevata sicurezza, proteggere la tua privacy e rendere Google ancora più efficace ed efficiente per te. Spendiamo centinaia di milioni di dollari ogni anno per la sicurezza e ci avvaliamo di esperti di fama mondiale in materia di sicurezza dei dati per mantenere le tue informazioni al sicuro. Abbiamo inoltre sviluppato strumenti per la sicurezza e la privacy di facile utilizzo come Google Dashboard, la verifica in due passaggi e Impostazioni annunci. Così, per quanto riguarda le informazioni che condividi con Google, hai il massimo controllo.</div> <div class='my-4'>
             
             Puoi leggere ulteriori informazioni sulla sicurezza online, incluso come proteggere te e la tua famiglia online, nel <a href='#'>Centro Google per la sicurezza online</a>.</div> <div class='my-4'>
             
             <a href='#'>Scopri</a> quali misure adottiamo per garantire protezione e sicurezza alle tue informazioni personali, lasciando a te il controllo.</div>"
-        ],
-        [
-            "faq" => "Perché il mio account è associato a un paese?",
+    ],
+    [
+        "faq" => "Perché il mio account è associato a un paese?",
 
-            "response" => "<div class='my-4'>Il tuo account è associato a un paese (o territorio) nei Termini di servizio per poter stabilire due cose:</div>
+        "response" => "<div class='my-4'>Il tuo account è associato a un paese (o territorio) nei Termini di servizio per poter stabilire due cose:</div>
 
             <ol class='response-ordered-list'>
                 <li class='ps-3'>
@@ -49,33 +49,41 @@
                 <li class='ps-3'>La versione dei termini che regola il nostro rapporto, che può variare in base alle leggi locali.</li>
             </ol>
             <div class='my-4'>Tieni presente che i servizi Google sono fondamentalmente gli stessi a prescindere dalla società consociata che li offre o dal paese a cui è associato il tuo account.</div>"
-        ],
-        [
-            "faq" => "Stabilire il paese associato al tuo account",
+    ],
+    [
+        "faq" => "Stabilire il paese associato al tuo account",
 
-            "response" => "<div class='my-4'> Quando crei un nuovo Account Google, lo associamo a un paese in base a dove è stato creato. Per quanto riguarda gli account creati almeno un anno fa, usiamo il paese da cui accedi solitamente ai servizi Google, in genere i servizi in cui hai trascorso più tempo nell'ultimo anno.</div> <div class='my-4'>
+        "response" => "<div class='my-4'> Quando crei un nuovo Account Google, lo associamo a un paese in base a dove è stato creato. Per quanto riguarda gli account creati almeno un anno fa, usiamo il paese da cui accedi solitamente ai servizi Google, in genere i servizi in cui hai trascorso più tempo nell'ultimo anno.</div> <div class='my-4'>
 
             I viaggi frequenti solitamente non influiscono sul paese associato al tuo account. Se ti trasferisci in un altro paese, potrebbe occorrere circa un anno per aggiornare l'associazione del paese.</div> <div class='my-4'>
             
             Se il paese associato al tuo account non corrisponde al tuo paese di residenza, il motivo potrebbe essere la differenza tra il paese in cui lavori e il paese in cui risiedi, l'installazione di una rete privata virtuale (VPN) per mascherare il tuo indirizzo IP oppure la residenza vicino a un confine territoriale. <a href='#'>Contattaci</a> se ritieni che il paese associato al tuo account sia sbagliato.</div>"
-        ],
-        [
-            "faq" => "Come faccio a rimuovere informazioni su di me dai risultati di ricerca di Google?",
+    ],
+    [
+        "faq" => "Come faccio a rimuovere informazioni su di me dai risultati di ricerca di Google?",
 
-            "response" => "<div class='my-4'> I risultati di ricerca di Google rispecchiano i contenuti pubblicamente disponibili sul Web. I motori di ricerca non possono rimuovere i contenuti direttamente dai siti web, quindi rimuovere risultati di ricerca da Google non consente di rimuovere i contenuti dal Web. Se desideri rimuovere qualcosa dal Web, devi <a href='#'>contattare il webmaster</a> del sito su cui sono pubblicati i contenuti e chiedergli di apportare una modifica. Inoltre, se, ai sensi delle leggi europee per la protezione dei dati, desideri richiedere la rimozione di determinate informazioni su di te visualizzate nei risultati di ricerca di Google, <a href='#'>fai clic qui</a>. Una volta che i contenuti saranno stati rimossi e che Google avrà rilevato l'aggiornamento, le informazioni non verranno più visualizzate nei risultati di ricerca di Google. In caso di una richiesta di rimozione urgente, è inoltre possibile <a href='#'>visitare la nostra pagina di assistenza per avere ulteriori informazioni</a>.</div>"
-        ],
-        [
-            "faq" => "Quando faccio clic sui risultati della Ricerca Google, le mie chiavi di ricerca vengono inviate ai siti web?",
+        "response" => "<div class='my-4'> I risultati di ricerca di Google rispecchiano i contenuti pubblicamente disponibili sul Web. I motori di ricerca non possono rimuovere i contenuti direttamente dai siti web, quindi rimuovere risultati di ricerca da Google non consente di rimuovere i contenuti dal Web. Se desideri rimuovere qualcosa dal Web, devi <a href='#'>contattare il webmaster</a> del sito su cui sono pubblicati i contenuti e chiedergli di apportare una modifica. Inoltre, se, ai sensi delle leggi europee per la protezione dei dati, desideri richiedere la rimozione di determinate informazioni su di te visualizzate nei risultati di ricerca di Google, <a href='#'>fai clic qui</a>. Una volta che i contenuti saranno stati rimossi e che Google avrà rilevato l'aggiornamento, le informazioni non verranno più visualizzate nei risultati di ricerca di Google. In caso di una richiesta di rimozione urgente, è inoltre possibile <a href='#'>visitare la nostra pagina di assistenza per avere ulteriori informazioni</a>.</div>"
+    ],
+    [
+        "faq" => "Quando faccio clic sui risultati della Ricerca Google, le mie chiavi di ricerca vengono inviate ai siti web?",
 
-            "response" => "<div class='my-4'> In alcuni casi sì. Quando fai clic su un risultato della Ricerca Google, il tuo browser web potrebbe reindirizzare alla pagina web di destinazione anche l'indirizzo Internet, o URL, della pagina dei risultati di ricerca sotto forma di URL referrer. Talvolta, l'URL della pagina dei risultati di ricerca potrebbe contenere la query di ricerca che hai inserito. Se utilizzi la ricerca SSL (la funzione di ricerca criptata di Google), nella maggior parte dei casi i termini di ricerca non vengono inviati come parte dell'URL negli <a href='#'>URL referrer</a>. Questo comportamento può fare eccezione, ad esempio se utilizzi alcuni browser meno diffusi. Ulteriori informazioni sulla ricerca SSL sono disponibili <a href='#'>qui</a>. Le query di ricerca o le informazioni contenute nell'URL referrer potrebbero essere disponibili mediante Google Analytics o un'API (Application Programming Interface). Inoltre, gli inserzionisti potrebbero ricevere informazioni relative all' esatte parole chiave che hanno determinato il clic su un annuncio.</div>"
-        ]
-    ];
+        "response" => "<div class='my-4'> In alcuni casi sì. Quando fai clic su un risultato della Ricerca Google, il tuo browser web potrebbe reindirizzare alla pagina web di destinazione anche l'indirizzo Internet, o URL, della pagina dei risultati di ricerca sotto forma di URL referrer. Talvolta, l'URL della pagina dei risultati di ricerca potrebbe contenere la query di ricerca che hai inserito. Se utilizzi la ricerca SSL (la funzione di ricerca criptata di Google), nella maggior parte dei casi i termini di ricerca non vengono inviati come parte dell'URL negli <a href='#'>URL referrer</a>. Questo comportamento può fare eccezione, ad esempio se utilizzi alcuni browser meno diffusi. Ulteriori informazioni sulla ricerca SSL sono disponibili <a href='#'>qui</a>. Le query di ricerca o le informazioni contenute nell'URL referrer potrebbero essere disponibili mediante Google Analytics o un'API (Application Programming Interface). Inoltre, gli inserzionisti potrebbero ricevere informazioni relative all' esatte parole chiave che hanno determinato il clic su un annuncio.</div>"
+    ]
+];
+
+$footerLinks = [
+    "Google",
+    "Tutto su Google",
+    "Privacy",
+    "Termini"
+];
 ?>
 
 
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -86,8 +94,9 @@
     <link rel="stylesheet" href="./style.css">
     <title>Document</title>
 </head>
+
 <body>
-    
+
     <header>
         <div class="d-flex justify-content-between px-4 py-2">
             <div>
@@ -95,14 +104,14 @@
             </div>
 
             <div class="d-flex"">
-                <div class="accounts-burger">
-                    <i class="fa-solid fa-dice-d20"></i>
-                </div>
-                
-                <div class="account">
-                    a
-                </div>
+                <div class=" accounts-burger">
+                <i class="fa-solid fa-dice-d20"></i>
             </div>
+
+            <div class="account">
+                a
+            </div>
+        </div>
         </div>
 
         <div>
@@ -133,5 +142,29 @@
             </div>
         </div>
     </main>
+
+    <footer>
+        <div class="faq-container py-3">
+            <div class="d-flex justify-content-between">
+                <div>
+                    <?php foreach ($footerLinks as $link) { ?>
+                        <a href="#">
+                            <?php echo $link ?>
+                        </a> -
+                    <?php } ?>
+                </div>
+
+                <div>
+                    <select name="lang" id="lang">
+                        <option value="it">Italiano</option>
+                        <option value="en">Inglese</option>
+                        <option value="fr">Francese</option>
+                    </select>
+                </div>
+            </div>
+        </div>
+
+    </footer>
 </body>
+
 </html>
