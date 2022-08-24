23723c62a15b563cfaf20fd5e4a5286105fc1707
AlessioCalabrese1 <alessiocalabrese07@gmail.com>
2022-08-24 15:49:36 +0200

    Header bozza

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css" integrity="sha512-1sCRPdkRXhBV2PBLUdRb4tMg1w2YPf37qatUFeS7zlBy7jJI8Lf4VHwWfZZfpXtYSLy85pkm9GaYVYMfw5BC1A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="./style.css">
+    <title>Document</title>
+</head>
+<body>
+    
+    <header>
+        <div class="d-flex justify-content-between px-4 py-2">
+            <div>
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1280px-Google_2015_logo.svg.png" alt="logo">
+            </div>
+
+            <div class="d-flex"">
+                <div class="accounts-burger">
+                    <i class="fa-solid fa-dice-d20"></i>
+                </div>
+                
+                <div class="account">
+                    a
+                </div>
+            </div>
+        </div>
+
+        <div>
+            <ul class="px-4">
+                <li class="py-2"><a href="#">Introduzione</a></li>
+                <li class="py-2"><a href="#">Norme sulla privacy</a></li>
+                <li class="py-2"><a href="#">Termini di servizio</a></li>
+                <li class="py-2"><a href="#">Tecnologie</a></li>
+                <li class="py-2"><a href="#">Domande frequenti</a></li>
+            </ul>
+        </div>
+    </header>
+
+</body>
+</html>
